acdaeebe5e1fe3d052325386dae0724c72fc82ba
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-10-28 21:31:08 +0400

    Implement user interface

--- src/src/components/Search.tsx
+++ src/src/components/Search.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { IoMdSearch } from "react-icons/io";
+import { useAppDispatch} from "../hooks/redux";
+import { fetchWeather } from "../store/actions/weatherActions";
 
 export function Search() {
-    return (
-        <div>serach</div>
-    )
+  const dispatch = useAppDispatch();
+  const [value, setValue] = useState("");
+
+  const changeHandler = (e: FormEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+  };
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    dispatch(fetchWeather(value));
+  };
+
+  return (
+    <form
+      className='w-full h-16 bg-black/20 max-w-[450px] rounded-full backdrop-blur-[32px] mb-6'
+      onSubmit={submitHandler}
+    >
+      <div className='h-full relative flex items-center justify-between p-2'>
+        <input
+          className='flex-1 bg-transparent outline-none placeholder:text-white text-white font-light pl-6 h-full '
+          type='text'
+          placeholder='Search by city'
+          value={value}
+          onChange={changeHandler}
+        ></input>
+        <button className='bg-[#9F62BB] hover:bg-[#8146A1] w-20 h-12 rounded-full flex justify-center items-center transition outline-none'>
+          <IoMdSearch className='text-2xl text-white' />
+        </button>
+      </div>
+    </form>
+  );
 }
--- src/src/pages/MainPage.tsx
+++ src/src/pages/MainPage.tsx
@@ -1,27 +1,45 @@
 import React, { useEffect } from "react";
-import { Navigation } from "../components/Navigation";
 import { Search } from "../components/Search";
 import { WeatherCard } from "../components/WeatherCard";
 import { useAppDispatch, useAppSelector } from "../hooks/redux";
-import { fetchWeather } from "../store/actions/weatherActions";
+import { fetchWeatherByCoordinates } from "../store/actions/weatherActions";
 
 export function MainPage() {
-  const dispatch = useAppDispatch()
-  
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(fetchWeather("chicago"))
-  }, [])
+    navigator.geolocation.getCurrentPosition((position) => {
+      dispatch(
+        fetchWeatherByCoordinates(
+          position.coords.latitude,
+          position.coords.longitude
+        )
+      );
+    });
+  }, [dispatch]);
 
-  const {data, loading, error} = useAppSelector(state => state.weather!)
+  const { error } = useAppSelector((state) => state.weather);
 
   return (
-    <div className='container mx-auto pt-10'>
-      <Navigation/>
+    <div className='w-full h-screen flex flex-col items-center justify-center bg-bgClouds bg-no-repeat bg-cover bg-center'>
+      {error && (
+        <div
+          className='w-full max-w-[450px] bg-[#ff208c] text-white absolute top-2 
+          lg:top-10 p-4 rounded-md text-center'
+        >
+          {error}
+        </div>
+      )}
 
-      <Search></Search>
+      {<Search></Search>}
 
-      <WeatherCard weatherData={data!}/>
+      {<WeatherCard />}
+      <a
+        className='text-white/30'
+        href='https://www.freepik.com/free-vector/evening-cloudy-sky-purple-background-with-group-cumulus-cirrus-clouds-flat-cartoon-illustration_16607919.htm#query=weather%20background&position=11&from_view=keyword'
+      >
+        Image by macrovector on Freepik
+      </a>
     </div>
   );
 }
